c6c39dab018c01f0f3d9948144a57baa6f999eac
Nelio Alves <acenelio@gmail.com>
2020-05-27 19:43:50 -0300

    Leitura do financiamento

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -11,7 +11,8 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		
 		String nome;
-		double salario;
+		double salario, porcentagemEntrada, valorFinanciado;
+		int prestacoes;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
@@ -22,6 +23,21 @@ public class Programa {
 		while (salario < 0) {
 			System.out.print("O salario nao pode ser negativo. Tente novamente: ");
 			salario = sc.nextDouble();
+		}
+		
+		System.out.print("Numero de prestacoes: ");
+		prestacoes = sc.nextInt();
+		System.out.print("Porcentagem de entrada: ");
+		porcentagemEntrada = sc.nextDouble();
+		System.out.print("Valor total financiado: ");
+		valorFinanciado = sc.nextDouble();
+		
+		double prestacao = valorFinanciado * (100.0 - porcentagemEntrada) / 100.0 / prestacoes;
+		
+		while (prestacao > 0.3 * salario) {
+			System.out.print("Capacidade de pagamento excedida! Tente novamente: ");
+			valorFinanciado = sc.nextDouble();
+			prestacao = valorFinanciado * (100.0 - porcentagemEntrada) / 100.0 / prestacoes;
 		}
 		
 		
